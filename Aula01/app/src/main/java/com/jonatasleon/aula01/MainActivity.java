fc998e46f2069146ce56a2c96df1e50b7d7c23a4
Jonatas Leon <jonatas.leon@gmail.com>
2016-04-09 20:41:52 -0300

    Aula 01 finalizada

---
+++ Aula01/app/src/main/java/com/jonatasleon/aula01/MainActivity.java
@@ -0,0 +1,29 @@
+package com.jonatasleon.aula01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText editText = (EditText) findViewById(R.id.editText);
+        final Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String saudacao = "Olá " + editText.getText().toString() + ", tudo bem?";
+                Toast.makeText(getBaseContext(), saudacao, Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
