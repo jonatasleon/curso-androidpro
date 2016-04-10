d79a58751eb7561edaa2720c330fc421334ce6a9
Jonatas Leon <jonatas.leon@gmail.com>
2016-04-10 17:27:06 -0300

    Aula03 finalizada

--- Aula03/app/src/main/java/com/jonatasleon/aula03/MainActivity.java
+++ Aula03/app/src/main/java/com/jonatasleon/aula03/MainActivity.java
@@ -2,6 +2,11 @@ package com.jonatasleon.aula03;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Animation fadein = AnimationUtils.loadAnimation(this, R.anim.fadein);
+        final ImageView iv = (ImageView) findViewById(R.id.imageView);
+        iv.startAnimation(fadein);
+
+        final TextView tv = (TextView) findViewById(R.id.textView);
+
+        final Animation fadeout = new AlphaAnimation(1f, 0);
+        fadeout.setDuration(2500);
+        fadeout.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                tv.startAnimation(fadeout);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        tv.startAnimation(fadeout);
     }
 }
