2f013a1fc53a54e66df3697480c34c99f4b04c83
Jonatas Leon <jonatas.leon@gmail.com>
2016-05-12 00:07:57 -0300

    Adiciona menu - Aula07

--- Aula07/app/src/main/java/com/jonatasleon/aula07/MainActivity.java
+++ Aula07/app/src/main/java/com/jonatasleon/aula07/MainActivity.java
@@ -2,6 +2,9 @@ package com.jonatasleon.aula07;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast toast = Toast.makeText(getApplicationContext(), "", Toast.LENGTH_LONG);
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                toast.setText("Action Settings");
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        toast.show();
+        return true;
     }
 }
